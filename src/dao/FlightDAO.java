365b73c93d3f5cf7e92a72bb61f85defcec35b30
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2025-02-14 15:25:34 +0300

    refactor: Refactor code remove unused imporation.

--- src/dao/FlightDAO.java
+++ src/dao/FlightDAO.java
@@ -1,7 +1,6 @@
 package dao;
 
 import model.Flight;
-import model.City;
 import utils.ConnectionBdd;
 
 import java.sql.*;
@@ -15,7 +14,7 @@ public class FlightDAO {
         String query = "INSERT INTO flight (departure_date, arrival_date, departure_city_id, arrival_city_id) VALUES (?, ?, ?, ?)";
 
         try (Connection conn = connectionBdd.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(query)) {
+                PreparedStatement pstmt = conn.prepareStatement(query)) {
 
             pstmt.setTimestamp(1, flight.getDeparture_date());
             pstmt.setTimestamp(2, flight.getArrival_date());
@@ -38,7 +37,7 @@ public class FlightDAO {
                 "WHERE f.flight_id = ?";
 
         try (Connection conn = connectionBdd.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(query)) {
+                PreparedStatement pstmt = conn.prepareStatement(query)) {
 
             pstmt.setInt(1, flightId);
 
@@ -64,8 +63,8 @@ public class FlightDAO {
         String query = "SELECT * FROM flight";
 
         try (Connection conn = connectionBdd.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(query);
-             ResultSet rs = pstmt.executeQuery()) {
+                PreparedStatement pstmt = conn.prepareStatement(query);
+                ResultSet rs = pstmt.executeQuery()) {
 
             while (rs.next()) {
                 Flight flight = new Flight(
@@ -87,7 +86,7 @@ public class FlightDAO {
                 "departure_city_id = ?, arrival_city_id = ? WHERE flight_id = ?";
 
         try (Connection conn = connectionBdd.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(query)) {
+                PreparedStatement pstmt = conn.prepareStatement(query)) {
 
             pstmt.setTimestamp(1, flight.getDeparture_date());
             pstmt.setTimestamp(2, flight.getArrival_date());
@@ -105,7 +104,7 @@ public class FlightDAO {
         String query = "DELETE FROM flight WHERE flight_id = ?";
 
         try (Connection conn = connectionBdd.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(query)) {
+                PreparedStatement pstmt = conn.prepareStatement(query)) {
 
             pstmt.setInt(1, flightId);
             pstmt.executeUpdate();
@@ -113,7 +112,8 @@ public class FlightDAO {
     }
 
     // Search
-    public  static List<Flight> searchFlights(int departureCityId, int arrivalCityId, Date departureDate) throws SQLException {
+    public static List<Flight> searchFlights(int departureCityId, int arrivalCityId, Date departureDate)
+            throws SQLException {
         ConnectionBdd connectionBdd = new ConnectionBdd();
         List<Flight> flights = new ArrayList<>();
         String query = "SELECT * FROM flight WHERE 1 = 1 ";
@@ -128,7 +128,7 @@ public class FlightDAO {
         }
 
         try (Connection conn = connectionBdd.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(query)) {
+                PreparedStatement pstmt = conn.prepareStatement(query)) {
 
             int i = 1;
             if (departureCityId != -1) {
