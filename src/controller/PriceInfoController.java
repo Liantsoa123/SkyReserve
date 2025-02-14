a167847855aa5668a1b2df3946589b107d039dcc
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2025-02-14 22:13:53 +0300

    feat: Create endPoint for to show the page insert price.

--- src/controller/PriceInfoController.java
+++ src/controller/PriceInfoController.java
@@ -1,4 +1,25 @@
 package controller;
 
+import dao.FlightDAO;
+import dao.SeatTypeDAO;
+import mg.noobframework.annotation.AuthMethod;
+import mg.noobframework.annotation.Controller;
+import mg.noobframework.annotation.Get;
+import mg.noobframework.annotation.Url;
+import mg.noobframework.modelview.Modelview;
+
+@Controller
 public class PriceInfoController {
+
+    @Get
+    @Url("/showInsertPriceInfo")
+    @AuthMethod("ADMIN")
+    public Modelview showInsertPriceInfo() throws Exception {
+        Modelview mv = new Modelview();
+        mv.add("seatTypes", SeatTypeDAO.findAll());
+        mv.add("flights", FlightDAO.findAll());
+        mv.setUrl("insertPriceInfo.jsp");
+        return mv;
+    }
+
 }
