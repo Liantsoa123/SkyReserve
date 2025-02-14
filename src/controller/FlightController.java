025571b3bf8f1691cbbcad3aed16c22a00e8ec3e
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2025-02-14 16:00:47 +0300

    feat: Return the value of the formulaire.

--- src/controller/FlightController.java
+++ src/controller/FlightController.java
@@ -23,7 +23,7 @@ public class FlightController {
     @Post
     @AuthMethod("ADMIN")
     @Url("/insertFlight")
-    public Modelview insertFlight(@RequestParamObject("Flight")Flight flight) {
+    public Modelview insertFlight(@RequestParamObject("Flight") Flight flight) {
         Modelview mv = new Modelview();
         mv.add("flight", flight);
         mv.add("url", "/showInsertFlight");
@@ -50,11 +50,15 @@ public class FlightController {
 
     @Post
     @Url("/searchFlights")
-    public Modelview searchFlights(@RequestParam("departureCityId")int departureCityId, @RequestParam("arrivalCityId")int arrivalCityId, Date departureDate) throws Exception {
+    public Modelview searchFlight(@RequestParam("departureCityId") int departureCityId,
+                                  @RequestParam("arrivalCityId") int arrivalCityId, @RequestParam("departureDate")Date departureDate) throws Exception {
         Modelview mv = new Modelview();
         mv.add("flights", FlightDAO.searchFlights(departureCityId, arrivalCityId, departureDate));
         mv.add("cities", CityDAO.findAll());
         mv.setUrl("listsFlight.jsp");
+        mv.add("departureCityId", departureCityId);
+        mv.add("arrivalCityId", arrivalCityId);
+        mv.add("departureDate", departureDate);
         return mv;
     }
 
