d47feda9b23daeb2e6ab0e65198e881dc8bcfc82
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2025-02-15 15:20:12 +0300

    feat: Create endpoint for insertion setting reservation.

--- src/controller/SettingReservationController.java
+++ src/controller/SettingReservationController.java
@@ -1,20 +1,43 @@
 package controller;
 
-import mg.noobframework.annotation.AuthClass;
-import mg.noobframework.annotation.Controller;
-import mg.noobframework.annotation.Get;
-import mg.noobframework.annotation.Url;
+import dao.SettingReservationDAO;
+import mg.noobframework.annotation.*;
 import mg.noobframework.modelview.Modelview;
+import model.SettingReservation;
 
 @Controller
 @AuthClass("ADMIN")
 public class SettingReservationController {
 
     @Get
-    @Url("/settingReservation")
+    @Url("/showSettingReservation")
     public Modelview showSettingReservation() {
         Modelview mv = new Modelview();
         mv.setUrl("settingReservation.jsp");
+        return mv;
+    }
+
+    @Post
+    @Url("/insertSettingReservation")
+    public Modelview insertSettingReservation(@RequestParamObject("SettingReservation") SettingReservation settingReservation) {
+        Modelview mv = new Modelview();
+        mv.add("settingReservation", settingReservation);
+        mv.add("url", "/showSettingReservation");
+        try {
+            //Update
+            if (settingReservation.getSetting_reservation_id() > 0) {
+                SettingReservationDAO.update(settingReservation);
+                mv.add("message", "Setting reservation updated successfully");
+            }
+            //Insert
+            else {
+                SettingReservationDAO.insert(settingReservation);
+                mv.add("message", "Setting reservation inserted successfully");
+            }
+
+        } catch (Exception e) {
+            mv.add("errorMessage", "An error occurred while inserting the setting reservation");
+        }
         return mv;
     }
 }
