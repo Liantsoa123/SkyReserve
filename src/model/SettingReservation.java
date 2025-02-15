0ca698bba94aa21cce6781648b4d0f86e00f8052
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2025-02-15 14:54:14 +0300

    feat: Create SettingReservation class.

---
+++ src/model/SettingReservation.java
@@ -0,0 +1,40 @@
+package model;
+
+public class SettingReservation {
+    private int setting_reservation_id;
+    private double reservation;
+    private double cancelation;
+
+    public SettingReservation() {
+    }
+
+    public SettingReservation(int setting_reservation_id, double reservation, double cancelation) {
+        this.setting_reservation_id = setting_reservation_id;
+        this.reservation = reservation;
+        this.cancelation = cancelation;
+    }
+
+    public int getSetting_reservation_id() {
+        return setting_reservation_id;
+    }
+
+    public void setSetting_reservation_id(int setting_reservation_id) {
+        this.setting_reservation_id = setting_reservation_id;
+    }
+
+    public double getReservation() {
+        return reservation;
+    }
+
+    public void setReservation(double reservation) {
+        this.reservation = reservation;
+    }
+
+    public double getCancelation() {
+        return cancelation;
+    }
+
+    public void setCancelation(double cancelation) {
+        this.cancelation = cancelation;
+    }
+}
