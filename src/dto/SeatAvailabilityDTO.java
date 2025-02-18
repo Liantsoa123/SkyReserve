014340f789d46dda470514d4091acdd6fd1cf424
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2025-02-18 22:50:28 +0300

    feat: Create a  SeatAvailabilityDTO.

---
+++ src/dto/SeatAvailabilityDTO.java
@@ -0,0 +1,63 @@
+package dto;
+
+public class SeatAvailabilityDTO {
+    private int seatTypeId;
+    private String typeName;
+    private int planeId;
+    private int totalSeats;
+    private int availableSeats;
+
+    // Constructeur par défaut
+    public SeatAvailabilityDTO() {
+    }
+
+    // Constructeur avec tous les champs
+    public SeatAvailabilityDTO(int seatTypeId, String typeName, int planeId, int totalSeats, int availableSeats) {
+        this.seatTypeId = seatTypeId;
+        this.typeName = typeName;
+        this.planeId = planeId;
+        this.totalSeats = totalSeats;
+        this.availableSeats = availableSeats;
+    }
+
+    // Getters et Setters
+    public int getSeatTypeId() {
+        return seatTypeId;
+    }
+
+    public void setSeatTypeId(int seatTypeId) {
+        this.seatTypeId = seatTypeId;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public int getPlaneId() {
+        return planeId;
+    }
+
+    public void setPlaneId(int planeId) {
+        this.planeId = planeId;
+    }
+
+    public int getTotalSeats() {
+        return totalSeats;
+    }
+
+    public void setTotalSeats(int totalSeats) {
+        this.totalSeats = totalSeats;
+    }
+
+    public int getAvailableSeats() {
+        return availableSeats;
+    }
+
+    public void setAvailableSeats(int availableSeats) {
+        this.availableSeats = availableSeats;
+    }
+}
