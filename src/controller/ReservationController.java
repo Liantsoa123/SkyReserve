023d786e79babc08821d85ca9f6fa4cfc18a53a0
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2025-02-19 13:18:56 +0300

    feat: Create the Endpoint for feature insertino reservation.

--- src/controller/ReservationController.java
+++ src/controller/ReservationController.java
@@ -1,19 +1,16 @@
 package controller;
 
+import java.sql.Timestamp;
 import java.util.List;
 
-import dao.CityDAO;
-import dao.FlightDAO;
-import dao.SeatAvailabilityDAO;
-import dao.SeatTypeDAO;
+import dao.*;
+import model.*;
+import org.postgresql.translation.messages_cs;
+
 import dto.SeatAvailabilityDTO;
-import mg.noobframework.annotation.AuthMethod;
-import mg.noobframework.annotation.Controller;
-import mg.noobframework.annotation.Get;
-import mg.noobframework.annotation.RequestParam;
-import mg.noobframework.annotation.Url;
+import mg.noobframework.annotation.*;
 import mg.noobframework.modelview.Modelview;
-import model.Flight;
+import utils.TimestampUtils;
 
 @Controller
 public class ReservationController {
@@ -28,9 +25,59 @@ public class ReservationController {
         mv.add("seatAvailability", seatAvailability);
         mv.add("flight", flight);
         mv.add("departure_city", CityDAO.findById(flight.getDeparture_city_id()));
-        mv.add("arrival_city" , CityDAO.findById(flight.getArrival_city_id()));
+        mv.add("arrival_city", CityDAO.findById(flight.getArrival_city_id()));
         mv.add("seatTypes", SeatTypeDAO.findAll());
         mv.setUrl("insertReservation.jsp");
+        return mv;
+    }
+
+    @Post
+    @Url("/insertReservation")
+    @AuthMethod("Client")
+    public Modelview insertReserve(@RequestParam("flight_id") int flightId , @RequestParam("seats_number") int seatsNumber, @RequestParam("seat_type_id")int seatypeId) throws Exception{
+        Modelview mv = new Modelview();
+
+        Flight flight = FlightDAO.findById(flightId);
+        List<SeatAvailabilityDTO> seatAvailability = SeatAvailabilityDAO.getAvailableSeats(flightId);
+        mv.add("seatAvailability", seatAvailability);
+        mv.add("flight", flight);
+        mv.add("departure_city", CityDAO.findById(flight.getDeparture_city_id()));
+        mv.add("arrival_city", CityDAO.findById(flight.getArrival_city_id()));
+        mv.add("seatTypes", SeatTypeDAO.findAll());
+        mv.setUrl("insertReservation.jsp");
+        mv.add("url", "/reserve");
+
+
+        //Check setting reservation
+        SettingReservation settingReservation = SettingReservationDAO.findById(1);
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        if (TimestampUtils.getHoursBetweenTimestamps(timestamp, flight.getDeparture_date()) < settingReservation.getReservation()) {
+            mv.add("errrorMessage", "Reservation Impossible pour ce vol");
+        }
+
+        SeatAvailabilityDTO seatAvailabilityDTO = SeatAvailabilityDAO.getAvailableSeatsBySeatId(flightId,seatypeId);
+
+        Reservation reservation = new Reservation();
+        NumberPromotion numberPromotion = NumberPromotionDAO.findByIdFlight(flightId);
+
+        //Check nombre place disponible
+        if ( seatsNumber > seatAvailabilityDTO.getAvailableSeats() ) {
+            mv.add("errorMessage", "Nombre de place insuffisant pour");
+        }
+        reservation.setSeats_number(seatsNumber);
+
+        //Check Promotion
+        if( seatAvailabilityDTO.getDiscountPercentage()>0 && seatAvailabilityDTO.getNumberPromotions() <= seatsNumber){
+            if (numberPromotion.getNumber() > 0 ){
+                reservation.setHas_promotion(true);
+                numberPromotion.setNumber( numberPromotion.getNumber()-1);
+            }
+        }
+
+        //if Reglo
+        ReservationDAO.insert(reservation);
+        NumberPromotionDAO.update(numberPromotion);
+
         return mv;
     }
 }
