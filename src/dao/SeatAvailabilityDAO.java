07b479f85e5322023a8e99b3b40cf8ef0e7575e4
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2025-02-19 14:52:26 +0300

    fix : Fix bug in the SeatAvailabilityDAO.

--- src/dao/SeatAvailabilityDAO.java
+++ src/dao/SeatAvailabilityDAO.java
@@ -18,13 +18,13 @@ public class SeatAvailabilityDAO {
         ConnectionBdd connectionBdd = new ConnectionBdd();
 
         String query = """
-                    WITH reserved_seats AS (
+                             WITH reserved_seats AS (
                     SELECT
                         seat_type_id,
                         SUM(seats_number) as reserved_count
                     FROM reservation
                     WHERE flight_id = ?
-                      AND reservation_status_id != (SELECT reservation_status_id FROM reservation_status WHERE reseravtion_name = 'Cancelled')
+                      AND reservation_status_id != (SELECT resrvation_status_id FROM reservation_status WHERE reseravtion_name = 'Annulé')
                     GROUP BY seat_type_id
                 )
                 SELECT
@@ -32,7 +32,7 @@ public class SeatAvailabilityDAO {
                     st.type_name,
                     ps.plane_id,
                     ps.number as total_seats,
-                    COALESCE(ps.number - rs.reserved_count, ps.number) as available_seats,
+                    (ps.number - rs.reserved_count) as available_seats,
                     pi.unit_price,
                     pi.discount_percentage,
                     pi.number as price_info_number
@@ -42,7 +42,7 @@ public class SeatAvailabilityDAO {
                          LEFT JOIN reserved_seats rs ON st.seat_type_id = rs.seat_type_id
                          LEFT JOIN price_info pi ON st.seat_type_id = pi.seat_type_id AND f.flight_id = pi.flight_id
                 WHERE f.flight_id = ?
-                                """;
+                """;
 
         try (Connection conn = connectionBdd.getConnection();
                 PreparedStatement pstmt = conn.prepareStatement(query)) {
@@ -75,30 +75,30 @@ public class SeatAvailabilityDAO {
         SeatAvailabilityDTO seatAvailabilityDTO = null;
 
         String query = """
-                    WITH reserved_seats AS (
-                    SELECT
-                        seat_type_id,
-                        SUM(seats_number) as reserved_count
-                    FROM reservation
-                    WHERE flight_id = ?
-                      AND reservation_status_id != (SELECT reservation_status_id FROM reservation_status WHERE reseravtion_name = 'Cancelled')
-                    GROUP BY seat_type_id
-                )
-                SELECT
-                    st.seat_type_id,
-                    st.type_name,
-                    ps.plane_id,
-                    ps.number as total_seats,
-                    COALESCE(ps.number - rs.reserved_count, ps.number) as available_seats,
-                    pi.unit_price,
-                    pi.discount_percentage,
-                    pi.number as price_info_number
-                FROM seat_type st
-                         JOIN plane_seats ps ON st.seat_type_id = ps.seat_type_id
-                         JOIN flight f ON f.plane_id = ps.plane_id
-                         LEFT JOIN reserved_seats rs ON st.seat_type_id = rs.seat_type_id
-                         LEFT JOIN price_info pi ON st.seat_type_id = pi.seat_type_id AND f.flight_id = pi.flight_id
-                WHERE f.flight_id = ? and st.seat_type_id = ?
+                WITH reserved_seats AS (
+                       SELECT
+                           seat_type_id,
+                           SUM(seats_number) as reserved_count
+                       FROM reservation
+                       WHERE flight_id = ?
+                         AND reservation_status_id != (SELECT resrvation_status_id FROM reservation_status WHERE reseravtion_name = 'Annulé')
+                       GROUP BY seat_type_id
+                   )
+                   SELECT
+                       st.seat_type_id,
+                       st.type_name,
+                       ps.plane_id,
+                       ps.number as total_seats,
+                       (ps.number - rs.reserved_count) as available_seats,
+                       pi.unit_price,
+                       pi.discount_percentage,
+                       pi.number as price_info_number
+                   FROM seat_type st
+                            JOIN plane_seats ps ON st.seat_type_id = ps.seat_type_id
+                            JOIN flight f ON f.plane_id = ps.plane_id
+                            LEFT JOIN reserved_seats rs ON st.seat_type_id = rs.seat_type_id
+                            LEFT JOIN price_info pi ON st.seat_type_id = pi.seat_type_id AND f.flight_id = pi.flight_id
+                   WHERE f.flight_id = ? and st.seat_type_id = ?
                                 """;
         try (Connection conn = connectionBdd.getConnection();
                 PreparedStatement pstmt = conn.prepareStatement(query)) {
