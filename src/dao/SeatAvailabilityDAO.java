0d70e99ddb528c3526350c173fc251934642875b
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2025-02-19 11:39:25 +0300

    feat: Create getAvailableSeatsBySeatId function.

--- src/dao/SeatAvailabilityDAO.java
+++ src/dao/SeatAvailabilityDAO.java
@@ -69,4 +69,60 @@ public class SeatAvailabilityDAO {
 
         return availabilityList;
     }
+
+    public static SeatAvailabilityDTO getAvailableSeatsBySeatId(int flightId, int idSeat) throws SQLException {
+        ConnectionBdd connectionBdd = new ConnectionBdd();
+        SeatAvailabilityDTO seatAvailabilityDTO = null;
+
+        String query = """
+                    WITH reserved_seats AS (
+                    SELECT
+                        seat_type_id,
+                        SUM(seats_number) as reserved_count
+                    FROM reservation
+                    WHERE flight_id = ?
+                      AND resrvation_status_id != (SELECT resrvation_status_id FROM reservation_status WHERE reseravtion_name = 'Cancelled')
+                    GROUP BY seat_type_id
+                )
+                SELECT
+                    st.seat_type_id,
+                    st.type_name,
+                    ps.plane_id,
+                    ps.number as total_seats,
+                    COALESCE(ps.number - rs.reserved_count, ps.number) as available_seats,
+                    pi.unit_price,
+                    pi.discount_percentage,
+                    pi.number as price_info_number
+                FROM seat_type st
+                         JOIN plane_seats ps ON st.seat_type_id = ps.seat_type_id
+                         JOIN flight f ON f.plane_id = ps.plane_id
+                         LEFT JOIN reserved_seats rs ON st.seat_type_id = rs.seat_type_id
+                         LEFT JOIN price_info pi ON st.seat_type_id = pi.seat_type_id AND f.flight_id = pi.flight_id
+                WHERE f.flight_id = ? and st.seat_type_id = ?
+                                """;
+        try (Connection conn = connectionBdd.getConnection();
+                PreparedStatement pstmt = conn.prepareStatement(query)) {
+
+            pstmt.setInt(1, flightId);
+            pstmt.setInt(2, flightId);
+            pstmt.setInt(3, idSeat);
+            try (ResultSet rs = pstmt.executeQuery()) {
+                while (rs.next()) {
+                    SeatType seatType = SeatTypeDAO.findById(rs.getInt("seat_type_id"));
+                    Plane plane = PlaneDAO.findById(rs.getInt("plane_id"));
+                    seatAvailabilityDTO = new SeatAvailabilityDTO(
+                            seatType,
+                            plane,
+                            rs.getInt("total_seats"),
+                            rs.getInt("available_seats"),
+                            rs.getDouble("unit_price"),
+                            rs.getDouble("discount_percentage"),
+                            rs.getInt("price_info_number"));
+                }
+            }
+        }
+
+        return seatAvailabilityDTO;
+
+    }
 }
